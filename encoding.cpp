9090c425935df4d60ddee659b928c6ef1a59f53e
Arathi V N <86509254+Arathi-V-N@users.noreply.github.com>
2021-10-24 19:27:05 +0530

    Update encoding.cpp

--- encoding.cpp
+++ encoding.cpp
@@ -1,7 +1,23 @@
-//this is a program to encode characters into numbers
+//program to convert character array to corresponding ASCII values
+
 #include<iostream>
 using namespace std;
 int main()
 {
-return 0;
+    char c[10];
+    int size;
+    cout << "Enter the size of character array:" << endl;
+    cin >> size;
+    cout << "Enter some characters:";
+    for(int i = 0;i < size ; i++)
+    {
+        cin >> c[i];
+    }
+    
+    cout << "The corresponding ASCII values are:";
+    for(int i = 0;i < size ; i++)
+    {
+        cout << int(c[i]) << " ";
+    }
+    return 0;
 }
